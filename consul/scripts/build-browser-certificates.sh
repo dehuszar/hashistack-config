fc81d95f4a4ebad7e90ad61376c5d650243ba1df
Samuel Allen <dehuszar@gmail.com>
2023-09-28 10:55:23 -0400

    locking in current state of scripts; could be some borkers in there

--- consul/scripts/build-browser-certificates.sh
+++ consul/scripts/build-browser-certificates.sh
@@ -2,6 +2,12 @@
 
 source /etc/envs/build-env-vars
 
+# Check for token existence before generating a new one
+if [[ -z "${VAULT_TOKEN}" ]]; then
+read client_token < <(echo $(vault login -method=oidc -format=json | jq -r '.auth.client_token'))
+export VAULT_TOKEN=$client_token
+fi
+
 sudo DOMAIN=$DOMAIN \
     CONSUL_DATACENTER=$CONSUL_DATACENTER \
     VAULT_ADDR=$VAULT_ADDR \
--- nomad/consul-template/configurations/client/templates/client.hcl.tpl
+++ nomad/consul-template/configurations/client/templates/client.hcl.tpl
@@ -4,4 +4,11 @@ client {
     driver.raw_exec.enable = "{{ env "NOMAD_OPTIONS_DRIVER_RAW_EXEC" }}"
     docker.privileged.enabled = "{{ env "NOMAD_OPTIONS_DOCKER_PRIVILEGED_ENABLED" }}"
   }
+
+  {{ range services }}
+host_volume "{{ .Name }}" {
+  path      = "{{ key "services/nomad/NOMAD_VOLUME_MOUNT_PATH" }}/{{ .Name }}"
+  read_only = false
+}
+  {{ end }}
 }
